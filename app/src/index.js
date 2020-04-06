d1eb964f3fe280f186fd9e5ce873dcb54bd06362
Rowan Ibrahim <rowan.ibrahim@yomicepa.com>
2020-04-06 21:50:14 +0200

    class component

--- app/src/index.js
+++ app/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
 
-const App = () =>{
+const AppFunction = () =>{
+
     return (
         <div className = "ui container comments">
+            <Segment>
             <ApprovalCard >
                 <CommentDetail author ="Rowan" timeAgo="Today at 4:45PM"/>
             </ApprovalCard>
+            </Segment>
+            
             <ApprovalCard >
                 <CommentDetail author ="Dalia" timeAgo="Today at 2:00PM"/>
             </ApprovalCard>
@@ -20,7 +24,53 @@ const App = () =>{
 }
 
 const Segment = (props) =>{
-    return(<div className = "ui placeholder segment"></div>);
+    return(<div className = "ui placeholder segment">
+        {props.children}
+    </div>);
 }
 
+class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            lat:null,
+            errorMessage: ''
+        }
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=> {
+                this.setState({
+                    lat: position.coords.latitude
+                })
+            },
+            (err)=>
+                this.setState({
+                errorMessage: err.message,
+            })
+        );
+    }
+    render(){
+        if(this.state.errorMessage && !this.state.lat){
+            return (
+                <div>
+                    Error: {this.state.errorMessage}
+                </div>
+                );
+        }
+        if(!this.state.errorMessage && this.state.lat){
+            return (
+                <div>
+                    Latitued: {this.state.lat} 
+                </div>
+                );
+        }else{
+            return(
+                <div>
+                    Loading! 
+                </div>
+            )
+        }
+        
+    }
+}
 ReactDOM.render(<App />, document.querySelector('#root'));
